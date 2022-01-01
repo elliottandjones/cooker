a1356c9230cc709e71087cf02a328dde7e90f4b9
Elliott <elliottohj44@gmail.com>
2021-12-31 20:51:35 -0500

    set up working prompt

--- src/commands/functional.ts
+++ src/commands/functional.ts
@@ -3,24 +3,27 @@ import { GluegunToolbox } from 'gluegun'
 module.exports = {
   name: 'functional',
   alias: ['f'],
+  descriptions: 'Creates a functional component',
   run: async (toolbox: GluegunToolbox) => {
     const {
       parameters,
       template: { generate },
-      print: { info }
+      print: { info },
     } = toolbox
 
     // console.log(parameters)
     const name = parameters.first || 'Component'
     const directory = parameters.second || null
-    const path = directory || name
+    let path = ''
+    directory ? (path = `${directory}${name}`) : (path = name)
+    const extension = path.includes('.js') || path.includes('.ts') ? '' : '.js'
 
     await generate({
       template: 'functional.js.ejs',
-      target: `${path}.js`,
-      props: { name }
+      target: `${path}${extension}`,
+      props: { name },
     })
 
-    info(`Generated file at ${path}`)
-  }
+    info(`Generated file at ${path}${extension}`)
+  },
 }
--- src/commands/react-cooker.ts
+++ src/commands/react-cooker.ts
@@ -1,3 +1,6 @@
+const FunctionalComponent = require('./functional')
+const ClassComponent = require('./class')
+
 module.exports = {
   name: 'cooker',
   run: async toolbox => {
@@ -8,16 +11,49 @@ module.exports = {
     const componentType = {
       type: 'select',
       name: 'type',
-      message: 'What type of component?',
-      choices: ['Class Component', 'Functional Component']
+      initial: 'Functional',
+      message: 'What type of Component? (Defaults to Functional Component)',
+      choices: [
+        { name: 'Functional', message: 'Functional', hint: 'creates a react functional component' },
+        { name: 'Class', message: 'Class', hint: 'creates a react class component' },
+      ],
     }
-    const componentName = {type: 'input', name: 'name', message: 'What name are we giving this Component?'}
-    const componentDir = {type: 'input', name: 'where', message: 'Where are we generating our Component?'}
-
+    const componentName = {
+      type: 'input',
+      name: 'name',
+      initial: 'Component',
+      message: 'What name are we giving this Component?',
+    }
+    const componentDir = {
+      type: 'input',
+      name: 'where',
+      initial: './',
+      message: 'Where are we generating our Component?',
+    }
     // ask a series of questions
-    const { name, type, where } = await toolbox.prompt.ask(componentType, componentName, componentDir)
-    console.log(`${name} ${type}, ${where}, Gooo!`)
-    // print.info('')
-  }
-}
+    const questions = [componentType, componentName, componentDir]
+    const { type, name, where } = await toolbox.prompt.ask(questions)
 
+    if (type === 'Functional') {
+      return FunctionalComponent.run({
+        ...toolbox,
+        parameters: {
+          first: name,
+          second: where,
+        },
+      })
+    }
+    if (type === 'Class') {
+      return ClassComponent.run({
+        ...toolbox,
+        parameters: {
+          first: name,
+          second: where,
+        },
+      })
+    }
+
+    console.log(`${name}, ${type}, ${where}, Gooo!`)
+    // print.info('')
+  },
+}
